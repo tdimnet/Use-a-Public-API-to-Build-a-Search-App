3258e85afedce83841a9eafed25e194288b15af9
Thomas Dimnet <Thomas.dimnet@gmail.com>
2017-04-25 18:01:48 +0200

    Change the search which matchs now the album and not the artists

--- js/app.js
+++ js/app.js
@@ -4,6 +4,10 @@ const searchInput = document.getElementById('search');
 
 
 
+/*************** Creating the html element to display ***************/
+
+
+
 /*************** The AJAX function ***************/
 function makeAJAXRequest(searchingText) {
   let xhr = new XMLHttpRequest();
@@ -13,17 +17,26 @@ function makeAJAXRequest(searchingText) {
       console.log('connected');
       if (xhr.status === 200) {
         let responseText = JSON.parse(xhr.responseText);
-        console.log(responseText);
+        let albumsArray = responseText.albums.items;
+
+        for (let i = 0; i < albumsArray.length; i++) {
+
+        }
+
+        console.log(albumsArray);
       } else {
         console.log('An error occured, sorry :/');
-      }
-    }
+      } // End: xhr.status
+    } // End: xhr.readyState
+  } // End: onreadystatechange
 
-  }
-
-  xhr.open('GET', "https://api.spotify.com/v1/search?q=" + searchingText + "&type=album", true);
+  xhr.open(
+    'GET',
+    "https://api.spotify.com/v1/search?q=album:" + searchingText + "&type=album&limit=10", // Limit the number for ten results
+    true
+  );
   xhr.send(null);
-}
+} // End: makeAJAXRequest()
 
 
 
